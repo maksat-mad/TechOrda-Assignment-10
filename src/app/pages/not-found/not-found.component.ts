24dd80aff2e764b518b2fd45bb1f6dae4bec4372
Maksat <maksat.madeniyetov@gmail.com>
2023-04-03 11:31:10 +0600

    chore: not-found page refactoring

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html'
+  template: '<div class="container">\n' +
+    '    <div class="d-flex flex-column justify-content-center align-items-center mt-5">\n' +
+    '      <h3 class="text-secondary">No such page</h3>\n' +
+    '      <a routerLink="/" class="text-decoration-none text-secondary-red-on-hover">Go to home page</a>\n' +
+    '    </div>\n' +
+    '</div>\n'
 })
 export class NotFoundComponent {
 
