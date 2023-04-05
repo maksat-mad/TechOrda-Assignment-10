e2cf0844430ff4782914db8b00776b066efa40da
Maksat <maksat.madeniyetov@gmail.com>
2023-04-06 00:44:19 +0600

    feat: image as modal

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,20 +1,25 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component, inject, OnInit, TemplateRef} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {MovieService} from "../../services/movie.service";
 import {Observable, switchMap} from "rxjs";
 import {Movie} from "../../modals/movie";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styles: [`.width-550 {
     width: 550px;
+  }
+  .cursor-pointer {
+    cursor: pointer;
   }`]
 })
 export class MovieComponent implements OnInit {
   route = inject(ActivatedRoute);
   router = inject(Router);
   movieService = inject(MovieService);
+  modalService = inject(NgbModal);
   movie: Observable<Movie | undefined> | undefined;
 
   ngOnInit(): void {
@@ -25,5 +30,9 @@ export class MovieComponent implements OnInit {
 
   noSuchMovie() {
     this.router.navigate(['not-found']);
+  }
+
+  openImageAsModal(content: TemplateRef<any>) {
+    this.modalService.open(content, { centered: true });
   }
 }
